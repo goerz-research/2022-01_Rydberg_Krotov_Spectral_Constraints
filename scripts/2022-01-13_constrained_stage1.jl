a2a8654cd00c6c3db79b982d045bfd321b8249d0
Michael Goerz <goerz@stanford.edu>
2022-01-13 23:03:19 +0100

    Add notebook for finding λₐ in stage 1

--- scripts/2022-01-13_constrained_stage1.jl
+++ scripts/2022-01-13_constrained_stage1.jl
@@ -18,7 +18,7 @@ function _convert_freq_to_float(s::String)
     if endswith(s, "GHz")
         return parse(Float64, s[1:end-3]) * (1/ns)
     else
-        @warn("Value $s taken to be in internal units. Consider '$sGHz' instead")
+        @warn("Value $s taken to be in internal units. Consider '$(s)GHz' instead")
         return parse(s)
     end
 end
@@ -26,13 +26,10 @@ end
 _convert_freq_to_float(v::Float64) = v
 
 
-function main(args=ARGS; λₐ=1e12, iter_stop=100, kwargs...)
+function main(args=ARGS; λₐ=1e12, iter_stop=100)
     # This takes ν_max as a command-line argument. Use "GHz" suffix, e.g.,
-    # `julia scripts/2022-01-13_constrained_stage1.jl 0.7GHz`
-    #
-    # For interactive usage, call `main([ν_max])`. In interactive mode only,
-    # we can also play around with the Krotov inverse step width λₐ, and other
-    # kwargs that will be forwarded to run_oct
+    # `julia scripts/2022-01-13_constrained_stage1.jl 0.7GHz`.
+    # For interactive usage, call `main([ν_max])`.
     ν_max = _convert_freq_to_float(args[1])
     oct_args = Dict(
         :iter_stop=>iter_stop,
@@ -42,7 +39,7 @@ function main(args=ARGS; λₐ=1e12, iter_stop=100, kwargs...)
     prefix = "oct_result"
     outdir = datadir("oct", "stage1")
     @produce_or_load(outdir, oct_args, prefix=prefix) do oct_args
-        oct_result = run_oct(;oct_args..., kwargs...)
+        oct_result = run_oct(;oct_args...)
         @strdict ν_min=0.0 ν_max oct_result
     end
 end
